c8013225989782ecd2bd0f19ea6f73ea8a783def
Devs <devangsuthar24@gmail.com>
2024-08-19 12:57:58 +0530

    new sections added

--- js/script.js
+++ js/script.js
@@ -4,13 +4,32 @@ $(jQuery(function(){
     $('body').css('padding-top',HeaderHi + 'px');
 
 
-    $(window).scroll(function() {    
-        var scroll = $(window).scrollTop();
-        
-        if (scroll >= 300) {
-            $('header').addClass('sticky');
-        } else {
-            $('header').removeClass('sticky');
-        }
-    });
+    $('.logo-slider').slick({
+        infinite: false,
+        dots: true,
+        arrows: false,
+        // autoplay: true,
+        // autoplaySpeed: 2000,
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+              }
+            },
+          ]
+      });
+
 }))
